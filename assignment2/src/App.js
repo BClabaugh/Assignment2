7d3e9e02a8bd40940b6dd5b137f753872c32238e
Blake Clabaugh <clab22@iastate.edu>
2024-04-01 21:22:29 -0500

    hehe name change

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -6,7 +6,7 @@ const render_products = (SheepHerd) => {
   return (
     <div className="category-section fixed">
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
-        Products ({SheepHerd.length})
+        Sheep ({SheepHerd.length})
       </h2>
       <div
         className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
