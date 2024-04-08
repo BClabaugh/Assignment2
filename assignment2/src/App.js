6f963aed09abe74b2d55f6cf275308647ce8503d
Zachack24 <150544086+Zachack24@users.noreply.github.com>
2024-04-07 21:58:34 -0500

    IT is DONE

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -5,99 +5,105 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import sheeps from "./data.json";
 
 const App = () => {
+  
+let search = "";
 const [cart, setCart] = useState([]);
 const [cartTotal, setCartTotal] = useState(0);
 const { register, handleSubmit, formState: { errors } } = useForm();
 const [dataF,setDataF] = useState({});
 const [viewer,setViewer] = useState(0);
-const [searchInput, setSearchInput] = useState("");
 
-const render_products =  sheeps.map((sheep) => (
-            <div>
-              <div key={sheep.id} className="group relative shadow-lg">
-                <div className=" min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-                <img
-                  src={sheep.image}
-                  alt={sheep.name}
-                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                />
-              </div>
-              <div className="flex justify-between p-3">
-                <div>
-                  <h3 className="text-sm text-gray-700">
-                    <a href={sheep.herf}>
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      <span style={{ fontSize: "16px", fontWeight: "600" }}>
-                        {sheep.name}
-                      </span>
-                    </a>
-                    <p>{sheep.breed}</p>
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">
-                    Weighs {sheep.weight}
-                  </p>
-                </div>
-                <p className="text-sm font-medium text-green-600">
-                  ${sheep.price}
-                </p>
-              </div>
-            </div>
-              <div className="bg-white rounded">
-              <button
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-                onClick={() => removeFromCart(sheep)}
-              >
-                {" "}
-                -{" "}
-              </button>{" "}
-              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={() => addToCart(sheep)}>
-                {" "}
-                +{" "}
-              </button>
-            </div>
-            <div className="bg-white rounded">
-              ${sheep.price} <span class="close">&#10005;</span>
-              {howManyofThis(sheep.id)}
-            </div>
-          </div>
-));
-const SearchBar = ({ sheeps }) => {
-  const [searchInput, setSearchInput] = React.useState('');
+const [searchTerm, setSearchTerm] = useState('');
 
-  const handleChange = (e) => {
-    setSearchInput(e.target.value);
-  };
-
-  const filteredSheep = searchInput.length > 0 ? sheeps.filter((sheep) => {
-    return sheep.name.toLowerCase().includes(searchInput.toLowerCase());
-  }) : sheeps;
-
-  return (
-    <div>
-      <input
-        type="search"
-        placeholder="Search here"
-        onChange={handleChange}
-        value={searchInput} 
-      />
-
-      <table>
-        <thead>
-          <tr>
-            <th>Sheep</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredSheep.map((sheep, id) => (
-            <tr key={id}>
-              <td>{sheep.name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+const handleChange = (event) => {
+  setSearchTerm(event.target.value);
 };
+
+const filteredSheeps = sheeps.filter((sheep) =>
+    sheep.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+const render_products = filteredSheeps.map((sheep) => (
+<div>
+  <div class="bg-gray-200 rounded-lg p-2" key={sheep.id}>
+    <div>
+      <img
+        src={sheep.image}
+        alt={sheep.name}
+        />
+    </div>
+
+    <div>
+      <h3>
+        <a href={sheep.herf}>
+          <span>{sheep.name}</span>
+        </a>
+
+        <p>{sheep.breed}</p>
+      </h3>
+
+      <p>
+        Weighs {sheep.weight}
+      </p>
+
+      <p>
+        ${sheep.price}
+      </p>
+
+     
+
+      <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full m-1" onClick={() => removeFromCart(sheep)}> - </button>
+      <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full m-1" onClick={() => addToCart(sheep)}> + </button>
+
+      <p>
+      ${sheep.price} <span class="close">&#10005;</span>
+      {howManyofThis(sheep.id)}
+      </p>
+
+    </div>
+  </div>
+</div>
+));
+
+const render_cart =  sheeps.filter(sheep => howManyofThis(sheep.id) > 0).map((sheep) => (
+  <div>
+     <div class="bg-gray-200 rounded-lg p-2" key={sheep.id}>
+    <div>
+      <img
+        src={sheep.image}
+        alt={sheep.name}
+        />
+    </div>
+
+    <div>
+      <h3>
+        <a href={sheep.herf}>
+          <span>{sheep.name}</span>
+        </a>
+
+        <p>{sheep.breed}</p>
+      </h3>
+
+      <p>
+        Weighs {sheep.weight}
+      </p>
+
+      <p>
+        ${sheep.price}
+      </p>
+
+      <p>
+      ${sheep.price} <span class="close">&#10005;</span>
+      {howManyofThis(sheep.id)}
+      </p>
+
+    </div>
+  
+  </div>
+
+</div>
+));       
+            
                 
    
 function howManyofThis(id) {
@@ -107,17 +113,18 @@ function howManyofThis(id) {
 const addToCart = (el) => {
   setCart([...cart, el]);
 };
+
 const removeFromCart = (el) => {
-  let hardCopy = [...cart];
-  hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-  setCart(hardCopy);
+  const index = cart.findIndex((cartItem) => cartItem.id === el.id);
+  if (index !== -1) {
+    const hardCopy = [...cart];
+    hardCopy.splice(index, 1);
+    setCart(hardCopy);
+  }
 };
-const cartItems = cart.map((el) => (
-  <div key={el.id}>
-    <img class="img-fluid" src={el.image} width={150} />
-    {el.title}${el.price}
-  </div>
-));
+
+
+
 useEffect(() => {
   total();
 }, [cart]);
@@ -130,14 +137,11 @@ const total = () => {
 };
 
 
+
   
 
-    function Start(s){
+    function Start(){
       
-        const updateHooks = () => {
-          setViewer( 1 );
-          setDataF({});
-        }
         return(
         <div className="flex fixed flex-row">
           <div
@@ -161,18 +165,36 @@ const total = () => {
                 <span style={{ color: "White" }}>Order total:</span>
                 <span style={{ color: "White" }}>${cartTotal}</span>
               </p>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={toCart}>Checkout</button>
               </div>
             </div>
           </div>
           <div className="ml-5 p-10 xl:basis-4/5">
           <div className="category-section fixed">
+        <div>
+        <div className="p-4 border border-gray-300 rounded">
+            <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleChange}
+            className="w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+      
+
+    </div>
+        </div>
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
           Sheep ({sheeps.length})
         </h2>
+
+
+        
         <div
           className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
           style={{ maxHeight: "800px", overflowY: "scroll" }}
         >
+      
             {render_products}
             </div>
             </div>
@@ -180,13 +202,19 @@ const total = () => {
           </div>
         </div>
         
-      );}
+    );}
+
       function Cart(){
+
+        const onSubmit = data => {
+          console.log( data ); // log all data
+          console.log( data.fullName ); // log only fullname
+          // update hooks
+          setDataF( data );
+          setViewer(2);
+      }
       
-        const updateHooks = () => {
-          setViewer( 2 );
-          setDataF({});
-        }
+
         return(
         <div className="flex fixed flex-row">
           <div
@@ -196,7 +224,7 @@ const total = () => {
             <div className="px-6 py-4">
               <h1 className="text-3xl mb-2 font-bold text-white">
                 {" "}
-                Sheep Auction{" "}
+                Cart / Trailer{" "}
               </h1>
               <p className="text-gray-700 text-white">
                 by -{" "}
@@ -210,6 +238,114 @@ const total = () => {
                 <span style={{ color: "White" }}>Order total:</span>
                 <span style={{ color: "White" }}>${cartTotal}</span>
               </p>
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={toBrowse}> Return to Browse</button>
+              <div>
+          <form onSubmit={handleSubmit(onSubmit) } className="container mt-5">
+            <div className="form-group">
+            <input {...register("fullName", { required: true })} placeholder="Full Name" className="form-control"/>
+            {errors.fullName && <p className="text-danger">Full Name is required.</p>}
+            </div>
+
+            <div className="form-group">
+            <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} placeholder="Email" className="form-control"/>
+            {errors.email && <p className="text-danger">Email is required.</p>}
+            </div>
+            <div className="form-group">
+            <input 
+                {...register("creditCard", { 
+                    required: true,
+                    pattern: /^\d{16}$/ 
+                })} 
+                type="tel"  
+                inputMode="numeric"  
+                placeholder="Credit Card" 
+                className="form-control"
+                maxLength={16}
+                minLength={16}
+                  
+            />
+            {errors.creditCard && <p className="text-danger">Credit Card is required and must be 16 digits.</p>}
+            </div>
+            <div className="form-group">
+            <input {...register("address", { required: true })} placeholder="Address" className="form-control"/>
+            {errors.address && <p className="text-danger">Address is required.</p>}
+            </div>
+            <div className="form-group">
+            <input {...register("city", { required: true })} placeholder="City" className="form-control"/>
+            {errors.city && <p className="text-danger">City is required.</p>}
+            </div>
+            <div className="form-group">
+            <input {...register("state", { required: true })} placeholder="State" className="form-control"/>
+            {errors.state && <p className="text-danger">State is required.</p>}
+            </div>
+            <div className="form-group">
+            <input {...register("zip", { required: true, pattern: /^\d{5}$/ })} placeholder="Zip" 
+              maxLength={5}
+              minLength={5}
+              className="form-control"/>
+            {errors.zip && <p className="text-danger">Zip is required.</p>}
+            </div>
+
+            <button type="submit" className="btn btn-primary">Order</button>
+
+            </form> 
+        </div>
+              </div>
+            </div>
+          </div>
+          <div className="ml-5 p-10 xl:basis-4/5">
+          <div className="category-section fixed">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
+          Cart ({sheeps.filter(sheep => howManyofThis(sheep.id) > 0).length})
+        </h2>
+        <div
+          className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
+          style={{ maxHeight: "800px", overflowY: "scroll" }}
+        >
+            {render_cart}
+            </div>
+            </div>
+       
+            
+          </div>
+
+         
+        </div>
+
+        
+      );}
+
+
+
+      function Checkout(){
+
+        
+
+        return(
+          
+        <div className="flex fixed flex-row">
+          
+          <div
+            className="h-screen bg-slate-800 p-3 xl:basis-1/5"
+            style={{ minWidth: "65%" }}
+          >
+            <div className="px-6 py-4">
+              <h1 className="text-3xl mb-2 font-bold text-white">
+                {" "}
+                Checkout{" "}
+              </h1>
+              <p className="text-gray-700 text-white">
+                by -{" "}
+                <b style={{ color: "White" }}>
+                  Blake Clabaugh, Zach Schmitz
+                </b>
+              </p>
+              {}
+              <div>
+              <p className="text-gray-700 text-white">
+               
+              </p>
+              <button onClick={newBrowser} className="btn btn-secondary">New Order</button>
               </div>
             </div>
           </div>
@@ -218,12 +354,27 @@ const total = () => {
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
           Sheep ({sheeps.length})
         </h2>
-        <searchBar sheeps={sheeps}/>
         <div
-          className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
+         
           style={{ maxHeight: "800px", overflowY: "scroll" }}
         >
-            {render_products}
+          <h1>Payment summary:</h1>
+            
+            <h3>{"Name: " + dataF.fullName}</h3>
+            <p>{"Email: " + dataF.email}</p>
+            <p>{"Credit Card ending in " + dataF.creditCard.substring(12)}</p>
+            <p>{"Address: " + dataF.address} {dataF.city}, {dataF.state} {dataF.zip} </p>
+            <span style={{ color: "Black" }}>Order total:</span>
+                <span style={{ color: "Green" }}>${cartTotal}</span>
+            <h1>Items Purchased</h1>
+             
+
+            
+         
+
+            </div>
+            <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10">
+            {render_cart}
             </div>
             </div>
             
@@ -231,11 +382,32 @@ const total = () => {
         </div>
         
       );}
+
+    
+
+
+      const newBrowser = () => {
+        window.location.reload();
+      }
+
+      const toBrowse = () => {
+        setViewer(0);
+      }
+
+      const toCart = () => {
+        setViewer(1);
+      }
+
+      const toConfirmation = () => {
+        setViewer(2);
+      }
+
 
       return(
         <div>
           {viewer === 0 && <Start />}
           {viewer === 1 && <Cart />}
+          {viewer === 2 && <Checkout />}
         </div>
       )
 
